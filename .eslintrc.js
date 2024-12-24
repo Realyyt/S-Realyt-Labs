2e71bb9c41f798f00ba4c21cd0f7c262e9f26892
realyyt <emmyisaac60@gmail.com>
2024-12-24 20:58:06 +0100

    feat

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+} 
