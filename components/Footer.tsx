80b6a8b0360083a9341fdff02d1bce70e61b7b3d
realyyt <emmyisaac60@gmail.com>
2025-01-07 12:24:29 +0100

    feat:dark-mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 dark:bg-gray-900">
+    <footer className="bg-gray-50 dark:bg-gray-800">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="md:col-span-3">
