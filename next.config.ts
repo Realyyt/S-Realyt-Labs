09736f81942a8303b6d6339b54815f1d7c2c0175
realyyt <emmyisaac60@gmail.com>
2025-02-05 13:58:14 +0100

    feat: output export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  // Enable if using static export
+  output: 'export' 
 };
 
 export default nextConfig;
