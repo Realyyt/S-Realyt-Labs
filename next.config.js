ff72a88a4fb5f97d137e3bfdab44650bfaba19ed
realyyt <emmyisaac60@gmail.com>
2025-03-21 12:56:59 +0100

    feat:

--- next.config.js
+++ next.config.js
@@ -10,7 +10,19 @@ const nextConfig = {
         source: '/api/:path*',
         destination: '/api/:path*',
       },
-    ];
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
   },
 }
 
